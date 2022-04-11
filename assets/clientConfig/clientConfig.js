d84cbbd5e8f1a97a8e3d5cb1cda6090a225f2839
tecplegithub <tecplegithub@gmail.com>
2022-04-11 23:00:27 +0530

    fc

---
+++ assets/clientConfig/clientConfig.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+      config_logo: "http://talnetglobal.xyz/assets/dist/img/logo.png",
+   // config_logo: "http://talnetglobal.xyz/assets/dist/img/tecple.png",
+    config_company_name:"TALNET",
+    config_company_fullname:"Tecple Talent Solutions (a division of Tecple Innoventive Solutions Pvt Ltd)",
+    config_website:"http://tecpleglobal.com"
+    
+  };
